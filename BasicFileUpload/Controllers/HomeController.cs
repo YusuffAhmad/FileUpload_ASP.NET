c934efa6eb28cd475a5ce777e570f4ec619c4072
YusuffAhmad <yusuffahmad2005@gmail.com>
2023-07-15 14:41:09 +0100

    added more connection strings

--- BasicFileUpload/Controllers/HomeController.cs
+++ BasicFileUpload/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
+using BasicFileUpload.Data;
 using BasicFileUpload.Gateway;
+using BasicFileUpload.Migrations;
 using BasicFileUpload.Models;
 using BasicFileUpload.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
@@ -9,10 +11,12 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly IFileServices _fileServices;
-    public HomeController(ILogger<HomeController> logger, IFileServices fileServices)
+    private readonly BasicFileUploadDbContext _context;
+    public HomeController(ILogger<HomeController> logger, IFileServices fileServices, BasicFileUploadDbContext context)
     {
         _logger = logger;
         _fileServices = fileServices;
+        _context = context;
     }
 
     public async Task<IActionResult> Index()
@@ -30,14 +34,31 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+
+    [HttpGet]
+    public IActionResult Privacy(int id)
     {
-        return View();
+        var file = GetFileDetails(id);
+
+        if (file == null)
+        {
+            return NotFound(); // File not found
+        }
+
+        return View(file);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    private FileModel GetFileDetails(int id)
+    {
+        // Retrieve the file from the database based on the provided ID
+        var file = _context.FileModels.Find(id);
+
+        return file;
     }
 }
